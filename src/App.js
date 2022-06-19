d5e726d93fc0d3fc2ec7b361fd2b0b134d0f172b
Andrew Gary <Andrew.gary91@gmail.com>
2022-06-19 03:08:08 -0500

    added AddNewProductForm.js component to the App.js component with routing to the page, I will create this page in the morning, goodnight

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Login from './components/Login';
 import ForgotPasswordPage from './components/ForgotPasswordPage';
 import { Routes, Route } from 'react-router-dom'
+import AddNewProductForm from './components/AddNewProductForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path='/addNewProduct' element={<AddNewProductForm />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forgotPassword' element={<ForgotPasswordPage />} />
       </Routes>
