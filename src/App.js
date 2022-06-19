3d14b78ccf55ffad8a8afd40f2c2ff7deb5deb20
Andrew Gary <Andrew.gary91@gmail.com>
2022-06-19 02:25:34 -0500

    styled/created the ForgotPasswordPage.js component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import WelcomeMessage from './components/WelcomeMessage';
 import Login from './components/Login';
+import ForgotPasswordPage from './components/ForgotPasswordPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <WelcomeMessage /> */}
-      <Login />
+      {/* <Login /> */}
+      <ForgotPasswordPage />
     </div>
   );
 }
