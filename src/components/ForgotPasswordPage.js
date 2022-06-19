77d655b26057febd899f86fb8a0524c7aa30f826
Andrew Gary <Andrew.gary91@gmail.com>
2022-06-19 03:02:56 -0500

    added state to the ForgotPasswordPage.js and created both handleChange and handleSubmit functions to the component, handleSubmit function still needs to be worked on once I build the backend

--- src/components/ForgotPasswordPage.js
+++ src/components/ForgotPasswordPage.js
@@ -1,6 +1,24 @@
 import { LockClosedIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+
+const initialFormValues = {
+    email: ''
+}
 
 const ForgotPasswordPage = () => {
+
+    const [ formValues, setFormValues ] = useState(initialFormValues);
+
+    const handleChange = e => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        console.log('Submit button clicked');
+    }
 
     return (
         <>
@@ -31,6 +49,7 @@ const ForgotPasswordPage = () => {
                   Email address
                 </label>
                 <input
+                  onChange={handleChange}
                   id="email-address"
                   name="email"
                   type="email"
@@ -46,6 +65,7 @@ const ForgotPasswordPage = () => {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={handleSubmit}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   {/* <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" /> */}
