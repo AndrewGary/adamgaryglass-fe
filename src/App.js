4213c0663f50f985f0b4d84de581e557a3770f33
Andrew Gary <Andrew.gary91@gmail.com>
2022-06-19 03:11:44 -0500

    added ShoppingCartPage.js component skeleton and created a Route for it in the App.js component, ok NOW im going to sleep

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import ForgotPasswordPage from './components/ForgotPasswordPage';
 import { Routes, Route } from 'react-router-dom'
 import AddNewProductForm from './components/AddNewProductForm';
+import ShoppingCartPage from './components/ShoppingCartPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path='/shoppingCart' element={<ShoppingCartPage />} />
         <Route path='/addNewProduct' element={<AddNewProductForm />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forgotPassword' element={<ForgotPasswordPage />} />
---
+++ src/components/ShoppingCartPage.js
@@ -0,0 +1,12 @@
+
+
+const ShoppingCartPage = () => {
+
+    return (
+        <div>
+            Shopping Cart page goes here!
+        </div>
+    )
+}
+
+export default ShoppingCartPage;
