eb886abf08bfb518bc83468429c748ed5b522043
Andrew Gary <Andrew.gary91@gmail.com>
2022-07-02 16:02:14 -0500

    Added a Footer.js component that will be applied to every page, still needs to be built out

---
+++ src/components/EmailListSignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EmailListSignUp = () => {
+    
+
+    return(
+        <div>
+            email List sign up
+        </div>
+    )
+}
+
+export default EmailListSignUp;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <div>
+            Footer goes here.
+        </div>
+    )
+}
+
+export default Footer;
